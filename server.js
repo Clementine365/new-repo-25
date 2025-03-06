5142c208cb7ef1355e9cdd37d82b203b9ceba047
Clementine.Iradukunda <clementineiradu365@gmail.com>
2025-03-06 12:30:39 +0200

    now

--- server.js
+++ server.js
@@ -6,11 +6,20 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-/* ***********************
+
+/* view Engine and Templates
+ * Routes
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ************************
  * Routes
  *************************/
 app.use(static)
